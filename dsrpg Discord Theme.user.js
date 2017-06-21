d5632837cc3b4bfaddd55efd8cf8fcfd27ab8207
Shamadruu <jacksonayres11@gmail.com>
2017-06-20 22:13:33 -0600

    Update dsrpg Discord Theme.user.js

--- dsrpg Discord Theme.user.js
+++ dsrpg Discord Theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DSRPG Discord-eqsue Theme
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        www.dsrpg.uk/*
